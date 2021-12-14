8f8fca6951261f434027655c8a9ca5db1e77f04f
Petre Vane <petre.vane@gmail.com>
2021-12-14 14:24:30 +0100

    Added tests

---
+++ src/test/java/com/orbsec/organizationservice/exceptions/CustomErrorHandlerTest.java
@@ -0,0 +1,30 @@
+package com.orbsec.organizationservice.exceptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+class CustomErrorHandlerTest {
+    @Test
+    void itShouldOrganizationErrorHandler() {
+        // Given
+        CustomErrorHandler customErrorHandler = new CustomErrorHandler();
+        MissingOrganizationException exception = new MissingOrganizationException("An error occurred");
+
+        // When
+        ResponseEntity<CustomError> actualOrganizationErrorHandlerResult = customErrorHandler
+                .organizationErrorHandler(exception);
+
+        // Then
+        assertTrue(actualOrganizationErrorHandlerResult.getHeaders().isEmpty());
+        assertTrue(actualOrganizationErrorHandlerResult.hasBody());
+        assertEquals(HttpStatus.NOT_FOUND, actualOrganizationErrorHandlerResult.getStatusCode());
+        CustomError body = actualOrganizationErrorHandlerResult.getBody();
+        assertEquals("An error occurred", body.getErrorMessage());
+        assertEquals(404, body.getStatusCode());
+    }
+}
+
